dd0a5b4c716a7f5b68869115ced99e9e04ed2831
xiaomei.wang <xiaomei.wang@aispeech.com>
2019-12-02 11:01:51 +0800

    common：包装发送告警的全局序列化类

---
+++ common/src/main/scala/com/aispeech/common/monitor/SparkJobMonitor.scala
@@ -0,0 +1,22 @@
+package com.aispeech.common.monitor
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * spark job 发送告警
+  *
+  * @author xiaomei.wang
+  * @date 2019/12/2 10:51
+  * @version 1.0
+  */
+object SparkJobMonitor extends Serializable {
+  def sendMessage(spark: SparkSession, message: String): Unit = {
+    val r = new SendUtils()
+    val conf = spark.sparkContext.getConf
+    conf.get("spark.monitor.urls")
+      .split(",")
+      .foreach(sendUrl =>
+        r.send(s"${spark.sparkContext.appName} is error. The message is ${message}",
+          sendUrl.trim))
+  }
+}
